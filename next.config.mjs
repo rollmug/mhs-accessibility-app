ffbd00cdae0d8b85c53db09fa95801b230ef3907
Dave Kobrenski <dave@bbmdesigns.com>
2025-03-14 10:42:30 -0400

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,18 @@ const nextConfig = {
                 hostname: 'rlmg-dev2.com',
                 port: '',
                 pathname: '/assets/**/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'directus',
+                port: '8055',
+                pathname: '/assets/**/**',
+            },
+            {
+                protocol: 'http',
+                hostname: '208.72.236.236',
+                port: '8055',
+                pathname: '/assets/**/**',
             }
         ],
     }
